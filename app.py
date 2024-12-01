1dca258ef1bd39fb01d58e4d93661590868d725c
Aime <a.ndayishim@alustudent.com>
2024-12-01 18:57:47 +0200

    Done

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
 import streamlit as st
 
 # Import pages
-from pages import (About_and_Documentation, Data_Visualizations, Home,
-                   Model_Prediction, Trigger_Retraining, Upload_and_Retrain)
+from pages import (Home, Data_Visualizations, Model_Prediction,
+                   Trigger_Retraining, About_and_Documentation)
 
 # Define pages
 PAGES = {
     "Home": Home.main,
-    "Model Prediction": Model_Prediction.main,
     "Data Visualizations": Data_Visualizations.main,
-    "Upload & Retrain": Upload_and_Retrain.main,
+    "Model Prediction": Model_Prediction.main,
     "Trigger Retraining": Trigger_Retraining.main,
     "About & Documentation": About_and_Documentation.main,
 }
